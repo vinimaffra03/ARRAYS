d974e2963cb7fb355a2fe5f0f5fd8249781e6f0f
vinimaffra03 <viniciushmdem15@gmail.com>
2025-01-26 20:40:57 -0300

    bugs callback

--- map-string.js
+++
@@ -1,7 +0,0 @@
-const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
-
-//arrow function no map com retorno
-const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
-// retorna um array com todos os elementos em caixa alta
-
-console.log(nomesPadronizados);
